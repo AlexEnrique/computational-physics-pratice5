6b6a87f27aecbcd391d842434f3c247fe61528aa
Alex Enrique <alex@alex-enrique.alex-enrique>
2018-07-14 19:35:37 -0300

    s

--- question-1/g_quadrature.h
+++ question-1/g_quadrature.h
@@ -6,39 +6,60 @@
 
 #define thetaD 428 // Kelvin
 #define pho 6.022E28 // m^{-3}
-#define volume 1000 //cm^3
+#define V 1000 //cm^3
 #define kB 1.38064852E-23 // m^3.Kg.s^{-2}.K^{-1} (SI)
 
 double Integrand(double x);
-double GaussianQuadrature(doouble (*f)(double x), double T);
+double CVGaussianQuadrature(doouble (*f)(double x), double T);
 void RescaleXInterval(double a, double b, double *x);
 void RescaleWInterval(double a, double b, double *w);
 
+// I have to count the number of steps of this func and see if it acceptable
 double *XSample(int N) {
-  double *x = malloc(N * sizeof(*x));
-  int k = 0;
-  double xi = -1;
-  double xf = 1;
-  double Pn, PnBefore;
-  PnBefore = -DBL_MAX
+  double xi, xf, dx, Pn, *x;
+  unsigned int k = 0; // index of x (xk)
+  short *sign = malloc(2 * sizeof(*sign)); // compare the last 2 element's sign
+  double PnBefore;
+
+  // Variables initializations
+  x = malloc(N * sizeof(*x)); // roots of Pn
+  xi = -1;
+  xf = 1;
+  dx = (xf-xi)/N;
+  PnBefore = -DBL_MAX // necessary? If not, exclude the <float.h> lib
+  sign[0] = (short)(gsl_sf_legendre_Pl(N, xi) > 0);
 
   while (xi <= xf && k < N) {
-    Pn = gsl_sf_legendre_Pl(N, x);
+    Pn = gsl_sf_legendre_Pl(N, xi);
     if (Pn == 0) {
-      x[k] = Pn;
+      x[k++] = Pn;
     }
-    else 
-
-
+    else {
+      sign[1] = (short)(Pn > 0);
+      if (sign[1] != sign[0]) {
+        // There exists a root between xi and xi-dx; find it
+        // ...
+        // Read GSL documentation for this part
+        x[k++] = // the root
+      }
+      sign[0] = sign[1];
+      // Problably the next line (also, PnBefore) will not be necessary
+      // PnBefore = Pn;
+    }
+    xi += dx;
   }
+
+  if (k < N-1) {
+    printf("Alguma raíz não fora encontrada, k: %d,  N: %d\n", k, N);
+  }
+
+  // Plot the Pn function and see if the roots are alright as a check
+  return x;
 }
 
 
 
-
-
-p
-double GaussianQuadrature(doouble (*f)(double x), double T) {
+double CVGaussianQuadrature(doouble (*f)(double x), double T) {
 
 
   return Cv;

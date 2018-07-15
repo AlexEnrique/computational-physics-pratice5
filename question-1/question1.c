9ffcb1ec8713875c7bc8d75714d69e240678728e
Alex Enrique <alex@alex-enrique.alex-enrique>
2018-07-15 12:26:39 -0300

    Final code

--- question-1/question1.c
+++ question-1/question1.c
@@ -2,32 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 #include "g_quadrature.h"
-#include <gsl/gsl_integration.h>
-
-
-double f(double x, void *par) {
-  return pow(sin(sqrt(100*x)),2);
-}
 
 int main() {
-  // Teste
-  double a = 0.0;
-  double b = 1.0;
-  double I;
-  const gsl_integration_fixed_type *T;
-  gsl_integration_fixed_workspace *w;
-  T = gsl_integration_fixed_legendre;
-  w = gsl_integration_fixed_alloc(T, 20, a ,b, 0, 0);
+  double T = 5, TMax = 500, dT = 0.1; // Kelvin
+  double Cv;
 
-  gsl_function func;
-  func.function = &f;
-  func.params = 0;
+  do {
+    Cv = CvQuadrature(T);
+    printf("%lf\t%1.5e\n", T, Cv);
 
-  gsl_integration_fixed(&func, &I, w);
-  printf("I; %1.20lf\n", I);
-
-
-  gsl_integration_fixed_free(w);
+    T += dT;
+  } while (T <= TMax);
 
   return 0;
 }
---
+++ question-1/script.gnu
@@ -0,0 +1,11 @@
+set key left top
+set xlabel 'Temperature (K)'
+set ylabel 'C_V (Kg.J/K)'
+
+plot 'temp.dat'  w l lt 7 title "Debye's Heat Capacity"
+
+set term eps
+set output 'DebyeHC.eps'
+replot
+
+unset term
